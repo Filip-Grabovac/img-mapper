7c024e6706ff27930f534bf629ec036115f70327
Filip-Grabovac <filipgrabovac55@gmail.com>
2023-08-21 20:57:06 +0200

    new tool

--- src/App.js
+++ src/App.js
@@ -7,11 +7,9 @@ import X from '../src/images/remove.png';
 function App() {
   const [shape, setShape] = useState(Mode.RECT);
   const [img, setImg] = useState();
-  const [seats, setSeats] = useState(0);
-  const [seatPrice, setSeatPrice] = useState(0);
-  const [seatsStatus, setSeatsStatus] = useState(false);
-  const [droppedItems, setDroppedItems] = useState([]);
-  const [db, setDb] = useState({ img: 'img.src' });
+  const [zoneInfo, setZoneInfo] = useState(false);
+  const [rowNum, setRowNum] = useState(1);
+  const [formData, setFormData] = useState();
 
   const handleImageUpload = (event) => {
     const selectedImage = event.target.files[0];
@@ -30,81 +28,27 @@ function App() {
       tagName === 'circle' ||
       tagName === 'rect'
     ) {
-      setSeatsStatus(true);
+      setZoneInfo(true);
       if (document.querySelector('.highlighted'))
         document.querySelector('.highlighted').classList.remove('highlighted');
       e.target.classList.add('highlighted');
     }
   }
 
-  function closeEdit() {
-    setSeatsStatus(false);
-    document.querySelector('.highlighted').classList.remove('highlighted');
-  }
-
-  function addSeats(e) {
-    e.preventDefault();
-
-    const numSeats = parseInt(e.target.elements.num_of_seats.value);
-    const seatPrice = parseInt(e.target.elements.seats_price.value);
-
-    setSeats(numSeats);
-    setSeatPrice(seatPrice);
-  }
-
-  const handleDragStart = (e, i) => {
-    e.dataTransfer.setData('text/plain', i);
-  };
-
-  const handleDrop = (e) => {
-    e.preventDefault();
-
-    const highlightedElement = document.querySelector('.highlighted');
-    if (!highlightedElement) return;
-
-    setSeats((seat) => seat - 1);
-
-    const itemId = e.dataTransfer.getData('text/plain');
-
-    // Get the dimensions and position of the containing element
-    const containerRect = e.currentTarget.getBoundingClientRect();
-
-    // Calculate the position relative to the containing element
-    const dropX = e.clientX - containerRect.left;
-    const dropY = e.clientY - containerRect.top;
-
-    // Get the dimensions and position of the highlighted element
-    const highlightedRect = highlightedElement.getBoundingClientRect();
-
-    // Replace these placeholders with the actual dimensions of your draggable element
-    const draggableElementWidth = 30; // Replace with actual width
-    const draggableElementHeight = 30; // Replace with actual height
-
-    // Calculate the nearest valid drop position within the boundaries of the highlighted element
-    const validDropX = Math.min(
-      Math.max(dropX, highlightedRect.left),
-      highlightedRect.right - draggableElementWidth
-    );
-
-    const validDropY = Math.min(
-      Math.max(dropY, highlightedRect.top),
-      highlightedRect.bottom - draggableElementHeight
-    );
-
-    // Add the dropped item to the droppedItems state
-    setDroppedItems((prevItems) => [
-      ...prevItems,
-      { id: itemId, left: validDropX, top: validDropY },
-    ]);
-  };
-
-  const handleDragOver = (e) => {
-    e.preventDefault();
-  };
-
   function setActiveLink(e) {
     document.querySelector('.active-link').classList.remove('active-link');
     e.target.classList.add('active-link');
+  }
+
+  function addZone(e) {
+    e.preventDefault();
+
+    console.log(document.querySelector('.highlighted'));
+
+    document.querySelector('.highlighted').classList.add('done');
+    document.querySelector('.highlighted').classList.remove('highlighted');
+    setShape(Mode.SELECT);
+    setZoneInfo(false);
   }
 
   return (
@@ -171,77 +115,58 @@ function App() {
                 Edit or add seats
               </a>
             </div>
-            <div
-              onDrop={handleDrop}
-              onDragOver={handleDragOver}
-              className="seats-container"
-            >
-              {droppedItems.map((item, i) => (
-                <div
-                  key={i}
-                  className="dropped-item"
-                  style={{ left: item.left, top: item.top }}
-                ></div>
-              ))}
-              <ImageMapperEditor
-                mode={shape}
-                cb={(editor) => {
-                  editor.loadImage(img, 700, 350);
-                  editor.on('mouseup', (e) => console.log('mouseup event', e));
-                  editor.polygon();
-                }}
-                options={{
-                  selectModeHandler: () =>
-                    console.log('Editor is now in select mode'),
-                  componentDrawnHandler: (shape, id) =>
-                    console.log(
-                      `${shape.element.tagName} with id ${id} is drawn. Call its freeze() function to disable selecting, deleting, resizing and moving.`
-                    ),
-                  viewClickHandler: () => {
-                    console.log('Clicked');
-                  },
-                  onclick: () => {
-                    console.log('Clicked');
-                  },
-                }}
-                handleShapeClick={handleShapeClick}
-              />
-            </div>
+            <ImageMapperEditor
+              mode={shape}
+              cb={(editor) => {
+                editor.loadImage(img, 700, 350);
+                editor.polygon();
+              }}
+              options={{}}
+              handleShapeClick={handleShapeClick}
+            />
           </>
         )}
       </div>
       <div>
-        {seatsStatus && (
+        {zoneInfo && (
           <>
-            <form className="seats-form" onSubmit={addSeats} action="">
-              <input
-                type="number"
-                name="num_of_seats"
-                placeholder="Unesite broj sjedala"
-              />
-              <input
-                type="number"
-                name="seats_price"
-                placeholder="Unesite cijenu sjedala"
-              />
-              <button>Dodaj</button>
-              <img onClick={closeEdit} src={X} alt="" />
-            </form>
-
-            <div className="seats-place">
-              {Array.from({ length: seats }).map((_, i) => {
+            <form
+              onSubmit={(e) => {
+                addZone(e);
+              }}
+              className="seats-form"
+              action=""
+            >
+              {Array.from({ length: rowNum }).map((_, i) => {
                 return (
-                  <div
-                    draggable
-                    className="seat"
-                    key={i}
-                    onDragStart={(e) => {
-                      handleDragStart(e, i);
-                    }}
-                  ></div>
+                  <div key={i}>
+                    <p>Red {i + 1}</p>
+                    <input
+                      className="num-of-seats"
+                      type="number"
+                      name="num_of_seats"
+                      placeholder={`Broj sjedala za red ${i + 1}`}
+                      data-row={i + 1}
+                    />
+                  </div>
                 );
               })}
-            </div>
+
+              <img
+                onClick={() => {
+                  setRowNum((rowNum) => rowNum + 1);
+                }}
+                src={Plus}
+                alt=""
+              />
+              <input
+                className="seats-price"
+                type="number"
+                name="seats_price"
+                placeholder={`Cijena sjelada u zoni`}
+              />
+              <button type="submit">Spremi zonu</button>
+            </form>
           </>
         )}
       </div>
